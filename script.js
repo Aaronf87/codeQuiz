cf9bae0bbd5075adb4cf454c91a4869e6d0c159b
Aaron Flores <aaron_flores08@yahoo.com>
2023-06-25 11:48:56 -0500

    added questions to question variable

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 // get element by ID into a variable in section
 // create a timer
 // create questions with multiple choice 
-// 
-// var nextQuestion = document.querySelector(question-number-display)
+let questions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choice1: "<script>",
+    choice2: "<ahref>",
+    choice3: "<br>",
+    choice4: "<link>",
+    Answer: "<script",
+ },
+  {
+    question: "what will the console output, console.log(500000 + 1)?",
+    choice1: "600000",
+    choice2: "500000 + 1",
+    choice3: "500001",
+    choice4: "5000001",
+    Answer: "500001",
+ },
+  {
+    question: "JavaScript adds ?",
+    choice1: "<script>",
+    choice2: "<ahref>",
+    choice3: "<br>",
+    choice4: "<link>",
+    Answer: "<script",
+ },
+
+]
+
+
+
+
 setInterval(myTimer, 1000);
 
 function myTimer() {
