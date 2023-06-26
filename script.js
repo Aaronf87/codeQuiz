2c204839bfca52be474578c92e97d3a24d73b478
Aaron Flores <aaron_flores08@yahoo.com>
2023-06-25 23:37:23 -0500

    added new bg image, adjusting js file

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // get element by ID into a variable in section
 // create a timer
 // create questions with multiple choice 
-  
+
+
     const question1 = {  
     question: "Inside which HTML element do we put the JavaScript?",
     choice1: "<script>",
@@ -12,23 +13,29 @@
  };
   
     const question2 =  {
-    Question: "what will the console output, console.log(500000 + 1)?",
+    question2: "what will the console output, console.log(500000 + 1)?",
     choice1: "600000",
     choice2: "500000 + 1",
     choice3: "500001",
     choice4: "5000001",
     Answer: "500001"
  };
+ let questionOne = document.querySelector("#question-number-display")
+//  let questionTwo = document.querySelector("question-number-display").innerHTML = question2.question2;
+
+questionOne.addEventListener('click', e =>{
 
 
-
-
-setInterval(myTimer, 1000);
-
-function myTimer() {
-  const d = new Date();
-  document.getElementById("time").innerHTML = d.toLocaleTimeString();
 }
+
+)
+
+// setInterval(myTimer, 1000);
+
+// function myTimer() {
+//   const d = new Date();
+//   document.getElementById("time").innerHTML = d.toLocaleTimeString();
+// }
 
 
 window.addEventListener("click", function(){
