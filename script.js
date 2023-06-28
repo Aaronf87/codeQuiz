8f613d434a92f1c7337c867e5fb66d2d999e4b81
Aaron Flores <aaron_flores08@yahoo.com>
2023-06-28 00:05:57 -0500

    created functions for each question

--- script.js
+++ script.js
@@ -20,24 +20,77 @@ let question1 = [ {
 
   
 var buttonElement = document.getElementById("button");
+var buttonElement2 = document.getElementById("button2");
+var buttonElement3 = document.getElementById("button3");
 var questionPelement = document.getElementById("question-number-display");
 var c1 = document.getElementById("c1");
 var c2 = document.getElementById("c2");
 var c3 = document.getElementById("c3");
 var c4 = document.getElementById("c4");
+qChoices = [questionInside,questionHow,questionWhich];
 
-
-
-buttonElement.addEventListener("click", function(){
+// let qInside = function questionInside() {
+//  // syntax used to acess the array object index 0
+//  questionPelement.innerHTML = question1[0].question;
+//  c1.innerHTML = question1[0].choices[0];
+//  c2.innerHTML = question1[0].choices[1];
+//  c3.innerHTML = question1[0].choices[2];
+//  c4.innerHTML = question1[0].choices[3];
+// };
+// let qWhich = function questionWhich() {
+//  // syntax used to acess the array object index 1
+//  questionPelement.innerHTML = question1[1].question;
+//  c1.innerHTML = question1[1].choices[0];
+//  c2.innerHTML = question1[1].choices[1];
+//  c3.innerHTML = question1[1].choices[2];
+//  c4.innerHTML = question1[1].choices[3];
+// };
+// let qHow = function questionHow() {
+//  // syntax used to acess the array object index 2
+//  questionPelement.innerHTML = question1[2].question;
+//  c1.innerHTML = question1[2].choices[0];
+//  c2.innerHTML = question1[2].choices[1];
+//  c3.innerHTML = question1[2].choices[2];
+//  c4.innerHTML = question1[2].choices[3];
+// };
+function questionInside() {
    // syntax used to acess the array object index 0
-   questionPelement.innerHTML = question1[0].question;
-                 c1.innerHTML = question1[0].choices[0];
-                 c2.innerHTML = question1[0].choices[1];
-                 c3.innerHTML = question1[0].choices[2];
-                 c4.innerHTML = question1[0].choices[3];
-  
-    console.log(question1[0].question);
-    console.log(question1[0].choices);
+   questionPelement.textContent = question1[0].question;
+   c1.innerHTML = question1[0].choices[0];
+   c2.innerHTML = question1[0].choices[1];
+   c3.innerHTML = question1[0].choices[2];
+   c4.innerHTML = question1[0].choices[3];
+  };
+function questionWhich() {
+   // syntax used to acess the array object index 1
+   questionPelement.innerHTML = question1[1].question;
+   c1.innerHTML = question1[1].choices[0];
+   c2.innerHTML = question1[1].choices[1];
+   c3.innerHTML = question1[1].choices[2];
+   c4.innerHTML = question1[1].choices[3];
+  };
+ function questionHow() {
+   // syntax used to acess the array object index 2
+   questionPelement.innerHTML = question1[2].question;
+   c1.innerHTML = question1[2].choices[0];
+   c2.innerHTML = question1[2].choices[1];
+   c3.innerHTML = question1[2].choices[2];
+   c4.innerHTML = question1[2].choices[3];
+  };
 
-  });
+  function start(){
+   if (true) {
+   
+function qArray(){
+   for(let i in qChoices){
+      return qChoices[i];
+
+   }}
+}
+   qArray();
+}
+
+buttonElement.addEventListener("click", questionInside);
+buttonElement2.addEventListener("click", questionWhich);
+buttonElement3.addEventListener("click", questionHow);
 
